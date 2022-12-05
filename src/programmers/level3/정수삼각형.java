9ca5f8e103417489fa3d01ba4167358b608567df
82108 <dudals789>
2022-12-05 23:18:22 +0900

    코딩 테스트 -LEVEL3 1문제 - dfs 풀이법

--- src/programmers/level3/정수삼각형.java
+++ src/programmers/level3/정수삼각형.java
@@ -2,21 +2,30 @@ package programmers.level3;
 
 public class 정수삼각형 {
 
+    static int max = 0;
+
     public int solution(int[][] triangle) {
-        int answer = 0;
 
-/*        triangle[0][0];
+        dfs(triangle, 0, 0, 0 );
 
-        triangle[1][0];
-
-        triangle[1][0];*/
-
-
-        return answer;
+        return max;
     }
 
-    public void dfs(int [][] triangle, int start, int end){
-        //triangle[start][start] + triangle[start+1][start]
+    public void dfs(int [][] triangle, int depth, int n, int sum){
+
+        sum += triangle[depth][n];
+
+
+        if(depth == triangle.length - 1){
+            if(sum > max){
+                max = sum;
+            }
+            return;
+        }
+
+        dfs(triangle, depth+1,  n, sum);
+        dfs(triangle, depth+1,  n+1, sum);
+
     }
 
 
@@ -27,5 +36,12 @@ public class 정수삼각형 {
     //4 5 2 6 5
     public static void main(String[] args) {
 
+        int [][] list ={{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
+
+        //System.out.println(list[4][4]);
+
+        정수삼각형 a = new 정수삼각형();
+
+        System.out.println(a.solution(list));
     }
 }
